f47e68898acdc71b90aeced6dea966917a6f8602
Vitaly Provodin <vitaly.provodin@jetbrains.com>
2021-04-09 05:24:54 +0700

    add Hello World class

---
+++ src/HellowWorld.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public void printHelloWorld() {
+        System.out.println("Hello, world");
+    }
+
+    public static void main(String[] args) {
+        (new Main()).printHelloWorld();
+    }
+}
